05db92f75204ee0c1acf78e4cf599a6c9c25c421
Brandon <BrandonPilane@gmail.com>
2025-05-17 10:41:32 +0200

    Add 'no subcommand' function

--- cents/cli.py
+++ cents/cli.py
@@ -1,1 +1,8 @@
 import click
+
+@click.group(invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        click.echo(f"Cents, a CLI for managing your finances.")
+        click.echo(cli.get_help(ctx))  # Show help message
