8c54e33b703f5c703573fd695394b12be1f0865f
Brandon <BrandonPilane@gmail.com>
2025-05-17 11:12:39 +0200

    feat: check for csv file

--- cents/cli.py
+++ cents/cli.py
@@ -1,11 +1,26 @@
 import click
 import csv
+from pathlib import Path
+
+data = Path.home() / "cents.csv" # Path to the CSV file
+
+def not_found():
+    """
+        Function to handle the case when the CSV file is not found.
+        It creates a new CSV file with the appropriate headers.
+    """
+    click.echo(f"No transactions found. Run \'"+ click.style("cents add", fg="green") + f"\' to add a new transaction.")
+    with open(data, "w") as f:
+        writer = csv.writer(f)
+        writer.writerow(["Id", "Date", "Description", "Amount", "Type"])
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def cli(ctx):
     if ctx.invoked_subcommand is None:
         click.echo(f"Cents, a CLI for managing your finances.")
+        if not data.exists():
+            not_found()
         click.echo(cli.get_help(ctx))  # Show help message
 
 @cli.command()
@@ -24,4 +39,3 @@ def add(desc, amount):
     
     """
     click.echo(f"Adding transaction: {desc} for {amount}")
-    
